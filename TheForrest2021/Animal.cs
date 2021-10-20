0ac48479abd4b3f667af292120f144adea036572
GoosePerfect <efraim.kirkhorn@gmail.com>
2021-10-20 11:43:33 +0200

    testtext lades till
    test

---
+++ TheForrest2021/Animal.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheForrest2021
+{
+    class Animal
+    {
+
+        public int Weight { get; set; }
+
+        public bool Nocturnal { get; set; }
+
+        public virtual void Move(bool nocturnal, bool day)
+        {
+            Console.WriteLine("Djuret är antingen vaket eller sover");
+        }
+
+    }
+
+    class Owl : Animal
+    {
+        public int Wingspan { get; set; }
+
+        public Owl(int weight, bool isNocturnal, int wingspan)
+        {
+            Wingspan = wingspan;
+            Weight = weight;
+            Nocturnal = isNocturnal;
+        }
+
+
+        public override void Move(bool nocturnal, bool day)
+        {
+            if(nocturnal == true && day == false)
+            {
+                Console.WriteLine("Ugglan flyger bland träden och letar mat");
+            }
+            else
+            {
+                Console.WriteLine("Ugglan sover i ett hål i trädet");
+            }
+        }
+    }
+
+    class Dolphin : Animal
+    {
+        public int FishPerDay { get; set; }
+
+        public Dolphin(int weight, bool isNocturnal, int fishPerDay)
+        {
+            FishPerDay = fishPerDay;
+            Weight = weight;
+            Nocturnal = isNocturnal;
+        }
+
+
+        public override void Move(bool nocturnal, bool day)
+        {
+            if (nocturnal == false && day == true)
+            {
+                Console.WriteLine("Delfinen simmar i sjön");
+            }
+            else
+            {
+                Console.WriteLine("Delfinen flyter sovande i sjön");
+            }
+        }
+
+    }
+
+    class Horse : Animal
+    {
+        public int HayPerDay { get; set; }
+
+        public Horse(int weight, bool isNocturnal, int hayPerDay)
+        {
+            HayPerDay = hayPerDay;
+            Weight = weight;
+            Nocturnal = isNocturnal;
+        }
+
+        public override void Move(bool nocturnal, bool day)
+        {
+            if (nocturnal == false && day == true)
+            {
+                Console.WriteLine("Hästen betar hö");
+            }
+            else
+            {
+                Console.WriteLine("Hästen sover i stallet");
+            }
+        }
+    }
+}
---
+++ TheForrest2021/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheForrest2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Dagtid eller Nattetid
+            bool day = true;
+
+
+            List<Animal> forrest = new List<Animal>();
+            forrest.Add(new Owl(7, true, 34));
+            forrest.Add(new Horse(740, false, 70));
+            forrest.Add(new Dolphin(432, false, 95));
+
+
+            while (true)
+            {
+                day = Menu(day);
+
+                foreach (Animal a in forrest)
+                {
+                    a.Move(a.Nocturnal, day);
+                    Console.WriteLine("Vikt: " + a.Weight);
+                    Console.WriteLine("Nattdjur: " + a.Nocturnal);
+
+                    if(a is Owl)
+                    {
+                        Console.WriteLine("Vingbredd: " + ((Owl)a).Wingspan + "cm");
+                    }
+
+                    if (a is Horse)
+                    {
+                        Console.WriteLine("Hö per dag: " + ((Horse)a).HayPerDay + "kg");
+                    }
+
+                    if (a is Dolphin)
+                    {
+                        Console.WriteLine("Fiskar per dag: " + ((Dolphin)a).FishPerDay + "st");
+                    }
+                }
+
+
+            }
+
+        }
+
+
+        public static bool Menu(bool day)
+        {
+            Console.WriteLine("Välj [D]ag eller [N]att\n");
+            Console.WriteLine($"Nu är det {(day ? "dag" : "natt")}");
+
+            string key = Console.ReadKey().Key.ToString();
+            switch (key)
+            {
+                case "D":
+                    day = true;
+                    break;
+                case "N":
+                    day = false;
+                    break;
+
+            }
+
+            Console.ForegroundColor = day ? ConsoleColor.Black : ConsoleColor.White;
+            Console.BackgroundColor = day ? ConsoleColor.White : ConsoleColor.Black;
+            Console.Clear();
+
+            return day;
+        }
+
+    }
+}
